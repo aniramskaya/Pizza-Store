4b60111562bdcb314403d551078cc3f1212b8043
Алексей <dev@MacBook-Pro-Aleksej.local>
2022-10-25 19:05:46 +0600

    Initial commit

---
+++ Pizza-Store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pizza-Store
+//
+//  Created by Алексей on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
